1b030e0018fe109629270388748121ee36965bdf
Freddy Ipanaque Castillo <fipanaque@synopsis.ws>
2020-11-05 13:01:06 -0500

    prepare to crc 2

--- src/main/java/com/telefonica/so/ShakaOffersApplication.java
+++ src/main/java/com/telefonica/so/ShakaOffersApplication.java
@@ -2,20 +2,12 @@ package com.telefonica.so;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class ShakaOffersApplication extends SpringBootServletInitializer{
+public class ShakaOffersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShakaOffersApplication.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ShakaOffersApplication.class);
-	}
-
 }
